9ae179cae42746f8ddad29f99c2564adfbd59b9a
Miguel Nogueira <me@miguelnogueira.com>
2016-10-07 18:21:41 +0100

    Lights API v0.01

---
+++ lights.py
@@ -0,0 +1,68 @@
+import machine, neopixel
+from time import sleep
+
+
+led_count = 12
+np = neopixel.NeoPixel(machine.Pin(4), led_count)
+
+# Colors
+
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+FUCSHIA = (255, 0, 255)
+GREEN = (0, 255, 0)
+MAGENTA = (0, 255, 255)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+
+def send_color(color_array):
+    for i in len(color_array):
+        np[i] = color_array[i]
+    np.write()
+
+
+def turn_off():
+    send_color(static_plain(BLACK))
+
+
+def static_plain(color):
+    return [color for i in range(led_count)]
+
+
+def static_alternate(color1, color2):
+    data = [color1 for i in range(led_count)]
+    for i in range(led_count):
+        if i % 2:
+            data[i] = color2
+    return data
+
+
+# def static_gradient(color1, color2):
+#     diff = ((color2[0] - color1[0])/led_count, (color2[1] - color1[1])/led_count, (color2[2] - color1[2])/led_count)
+#     data = []
+#     for i in range(led_count):
+#         data[i] = color1 + diff * i
+#     return data
+
+
+def list_rotate(lst):
+    copy = list(lst)
+    for i in range(len(lst)):
+        lst[i] = copy[i-1]
+    return lst
+
+
+def rotate(ring_color_data, led_count=12, rotations=5):
+    data = ring_color_data
+    for i in led_count * rotations:
+        send_color(data)
+        data = list_rotate(data)
+        sleep(0.5)
+
+
+c = send_color(static_alternate(BLUE, YELLOW))
+sleep(1)
+rotate(c)
+turn_off()
